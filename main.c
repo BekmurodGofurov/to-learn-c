d2a8990c2bb5ba856ae3c3184c8eb244b01f40b1
Bekmurod <bekmruodgofurov0000@gmail.com>
2024-11-03 10:25:10 +0500

    while

--- main.c
+++ main.c
@@ -1,29 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct student {
-    char name[20];
-    char major[30];
-    int age;
-    double gpa;
-};
-
-
 int main()
 {
-    struct student student1;
-    student1.age = 18;
-    student1.gpa = 4.7;
-    strcpy( student1.name, "Bekmurod");
-    strcpy( student1.major, "Cyber securty");
+    int index = 1;
+    while (index <= 5)
+    {
+        printf("%d\n", index);
+        // index = index + 1;
+        index++;
+    }
     
-    struct student student2;
-    student2.age = 19;
-    student2.gpa = 4.0;
-    strcpy( student2.name, "Jaloliddin");
-    strcpy( student2.major, "Business");
-
-    printf("%ss\n", student2.name);
     return 0;
 }
 
