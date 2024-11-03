d8e6523aefaa6a695f70f88da9b9288eb6a1c7a2
Bekmurod <bekmruodgofurov0000@gmail.com>
2024-11-03 21:20:03 +0500

    compex array

--- main.c
+++ main.c
@@ -3,12 +3,22 @@
 
 int main()
 {
-    int luckyNumbers[] = {2,4,6, 8, 18,20,33,80, 88,66};
-    int i;
-    for (i = 0; i < 10; i++)
+    int myFoNu[3][3] = {
+        {8, 2, 20},
+        {5, 7, 77},
+        {1, 10, 11}};
+
+    int i, j;
+
+    for (i = 0; i < 3; i++)
     {
-        printf("%d\n", luckyNumbers[i]);
+        for (j = 0; j < 3; j++)
+        {
+            printf("%d,", myFoNu[i][j]);
+        }
+
+        printf("\n");
     }
-    
+
     return 0;
 }
