8af4b6170d3b4221c3eaf2b68464b4eaf1492c18
Bekmurod <bekmruodgofurov0000@gmail.com>
2024-11-03 10:17:26 +0500

    student2

--- main.c
+++ main.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct student {
+    char name[20];
+    char major[30];
+    int age;
+    double gpa;
+};
+
+
 int main()
 {
-    char name[20];
-    printf("Enter your name: ");
-    fgets(name, 20, stdin);
-    printf("Hello\n");
-    printf("your name is %s.\n", name);
+    struct student student1;
+    student1.age = 18;
+    student1.gpa = 4.7;
+    strcpy( student1.name, "Bekmurod");
+    strcpy( student1.major, "Cyber securty");
+    
+    struct student student2;
+    student2.age = 19;
+    student2.gpa = 4.0;
+    strcpy( student2.name, "Jaloliddin");
+    strcpy( student2.major, "Business");
+
+    printf("%ss\n", student2.name);
     return 0;
 }
+
