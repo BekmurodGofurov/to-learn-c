835291fca25fd131620c4b217f939b9ee59bdbc5
Bekmurod <bekmruodgofurov0000@gmail.com>
2024-11-03 20:52:10 +0500

    for loop

--- main.c
+++ main.c
@@ -3,34 +3,12 @@
 
 int main()
 {
-    int secretNumber = 8;
-    int guess;
-    int guessCount = 0;
-    int guessLimit = 3;
-    int outOfGuess = 0;
-
-    while (guess != secretNumber && outOfGuess == 0)
+    int luckyNumbers[] = {2,4,6, 8, 18,20,33,80, 88,66};
+    int i;
+    for (i = 0; i < 10; i++)
     {
-        if (guessCount < guessLimit)
-        {
-            printf("Enter your gess number: ");
-            scanf("%d", &guess);
-            guessCount++;
-        }
-        else
-        {
-            outOfGuess = 1;
-        }
+        printf("%d\n", luckyNumbers[i]);
     }
-
-    if (outOfGuess == 1)
-    {
-        printf("You lose the game!\n");
-    }
-    else
-    {
-        printf("You Win!\n\n\n");
-    }
-
+    
     return 0;
 }
